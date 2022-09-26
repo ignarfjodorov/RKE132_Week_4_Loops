f0364c5b3a79a8e19288083976b2f37e8f0a707f
ignarfjodorov <ignar.fjodorov@gmail.com>
2022-09-26 19:46:21 +0300

    add week_4 code

---
+++ Week_4_Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿//FOR-LOOP
+//DRY-principle - Do not Repeat Yourself
+
+int i = 0;
+
+for(i = 0; i < 5; i++) //i++ on sama mis i + 1
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
+
+
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
